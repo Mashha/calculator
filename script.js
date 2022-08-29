a87ddc2e87071cb99c6c2242e8dedc49132903b0
Masa Orel <orel.masha89@gmail.com>
2022-08-29 17:02:03 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// operations
+
+function add(a, b) {
+    return a + b
+  }
+  
+  function subtract(a, b) {
+    return a - b
+  }
+  
+  function multiply(a, b) {
+    return a * b
+  }
+  
+  function divide(a, b) {
+    return a / b
+  }
+
+  // function that calls operations
+  
+  function operate(operator, a, b) {
+    switch (operator) {
+      case '+':
+        return add(a, b)
+      case '-':
+        return subtract(a, b)
+      case '*':
+        return multiply(a, b)
+      case '/':
+        return divide(a, b)
+    }
+  }
+  
+  console.log(operate('+', 20, 12))
