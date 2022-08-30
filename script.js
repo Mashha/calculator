c5b1f56b1e57ae3260fba663dc6eb324150668de
Masa Orel <orel.masha89@gmail.com>
2022-08-30 13:51:15 +0200

    added classes - HTML and Js - event listener on btn

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 // operations
 
 function add(a, b) {
-    return a + b
-  }
-  
-  function subtract(a, b) {
-    return a - b
-  }
-  
-  function multiply(a, b) {
-    return a * b
-  }
-  
-  function divide(a, b) {
-    return a / b
-  }
+  return a + b
+}
 
-  // function that calls operations
-  
-  function operate(operator, a, b) {
-    switch (operator) {
-      case '+':
-        return add(a, b)
-      case '-':
-        return subtract(a, b)
-      case '*':
-        return multiply(a, b)
-      case '/':
-        return divide(a, b)
-    }
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+// function that calls operations
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '-':
+      return subtract(a, b)
+    case '*':
+      return multiply(a, b)
+    case '/':
+      return divide(a, b)
   }
-  
-  console.log(operate('+', 20, 12))
+}
+
+// click on the number buttons and make them display on the screen
+
+let buttonNumbers = document.querySelectorAll('.number')
+let inputDisplay = document.getElementById('display')
+let storingValues = []
+
+buttonNumbers.forEach(function (button) {
+  button.addEventListener('click', (e) => {
+    inputDisplay.value += e.target.value
+  })
+})
+
+storingValues.push(inputDisplay.value)
